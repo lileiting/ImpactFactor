8ed642a05726ec9d11427c84f28478edb0a5a51e
Leiting Li <lileiting@gmail.com>
2016-06-26 22:22:21 +0800

    rewrite the codes

--- sci.pl
+++ sci.pl
@@ -2,79 +2,69 @@
 
 use warnings;
 use strict;
-use File::Spec;
 use FindBin;
 
 sub usage{
+    print <<"end_of_usage";
 
-print <<USAGE;
+Usage
+    $FindBin::Script [pattern]
 
-$FindBin::Script PATTERN
+Description
+    This is a program to list sci journals and their impact 
+    factor. The given pattern is considered as Perl regular 
+    expression, see the following examples.
 
-  This is a program to list sci journals and their impact 
-  factor. The JOURNAL NAME PATTERN is actually considered 
-  as Perl regular expression, see the following examples.
+    $FindBin::Script plant       # Means journals contain 'plant'
+    $FindBin::Script 'agri|hort' # Means journal names contain 'agri' or 'hort'
+    $FindBin::Script .           # Means all journals   
+    $FindBin::Script ^nat        # Means journals start with 'nat'
+    $FindBin::Script sci\$        # Means journals end with 'sci'
 
-  $FindBin::Script plant       # Means journals contain 'plant'
-  $FindBin::Script 'agri|hort' # Means journal names contain 'agri' or 'hort'
-  $FindBin::Script .           # Means all journals   
-  $FindBin::Script ^nat        # Means journals start with 'nat'
-  $FindBin::Script sci\$       # Means journals end with 'sci'
-
-USAGE
+end_of_usage
     exit;
 }
 
-sub get_pattern{
-    usage unless @ARGV;
-    return $ARGV[0];
-}
-
-sub sci_data_file_name{
-    my $filename = q/data_sci_2014.csv/;
-    my $fullname = File::Spec->catfile($FindBin::RealBin, $filename);
-    return $fullname;
-}
-
-sub load_sci_data{
-    my @journal;
-    my $scidata = sci_data_file_name;
+sub main{
+    usage unless @ARGV == 1;
+    my $pattern = shift @ARGV;
+    my @results;
+    print STDERR "----Searching: $pattern\n";
+    
+    my $scidata = "$FindBin::RealBin/data_sci_2014.csv";
     open my $fh, "<", $scidata or die "$scidata: $!";
-    my $title=<$fh>;
+    
+    # title
+    my $title = <$fh>;
     chomp($title);
-    $journal[0]=[(split(/,/,$title))];
+    my @title = split(/,/, $title);
+
+    # journal data
     while(<$fh>){
         chomp;
         my @txt = split(/,/);
-        unless($txt[4]){$txt[4] = 0}
-        $journal[$txt[0]]=[@txt];
-    }
-    close $fh;
-    return($title, \@journal);
-}
-
-sub grep_journal{
-    my ($re, $title, $ref) = @_;
-    my @journal = @$ref;
-
-    my @results;
-    print STDERR "----Searching: $re\n";
-    foreach(1..$#journal){
-        if($journal[$_]->[1] =~ /$re/i){
-            push @results,$journal[$_];
+        
+        # impact factor
+        $txt[4] = 0 unless $txt[4];
+        
+        # Journal title
+        if($txt[1] =~ /$pattern/i){
+            push @results,[@txt];
         }
     }
-    die "----WARNING: No journals found with names contain the text \"$re\"! Please specify another keyword.\n" 
+    close $fh;
+
+    die "----WARNING: No journals found with names contain the text \"$pattern\"! Please specify another keyword.\n" 
         unless @results;
     my $count = 0;
-    foreach(sort{$b->[4] <=> $a->[4]}@results){
+    for(sort{$b->[4] <=> $a->[4]}@results){
         $count++;
-        printf("%4s:|IF=%8.3f |Articles=%6d |ISSN=$_->[2]|Name=$_->[1]\n",$count,$_->[4],$_->[7]?$_->[7]:0);
+        printf("%4s:|IF=%8.3f |Articles=%6d |ISSN=$_->[2]|Name=$_->[1]\n",
+            $count,$_->[4],$_->[7]?$_->[7]:0);
     }
-}
-
-sub main{
-    grep_journal(get_pattern(), load_sci_data());
+    
 }
 
 main() unless caller;
+
+__END__
